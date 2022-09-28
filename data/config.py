4c3bcdc5dd6c033f5421740a307d8aedcbd90620
Александр <Aleksand.Yaroslavsky@x5.ru>
2022-09-28 12:58:13 +0200

    забыл ебаный токен поменять

--- data/config.py
+++ data/config.py
@@ -1,5 +1,5 @@
 test = '5774552772:AAHCkj9dY7gu-WWAIN0ELu42KrqB_YCnn8c'
 real = '5223377564:AAF_QQzyd9Y_BnkX3Vb_54R4JhmwNGKfuCQ'
 DATABASE_NAME = ''
-TOKEN = test
+TOKEN = real
 APP_URL = 'https://careerweek.herokuapp.com/' + TOKEN
