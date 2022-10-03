bfe61ffd2514466c33f1d47aec94d7e703fdac36
MadMan911 <90758015+MadMan911@users.noreply.github.com>
2022-10-03 17:35:34 +0300

    Update config.py

--- data/config.py
+++ data/config.py
@@ -1,5 +1,5 @@
 test = '5774552772:AAHCkj9dY7gu-WWAIN0ELu42KrqB_YCnn8c'
-real = '5223377564:AAF_QQzyd9Y_BnkX3Vb_54R4JhmwNGKfuCQ'
+real = '5223377564:AAH3Vi3z7gW8Zc3y9omM8xa7B9p6iFdyZTw'
 DATABASE_NAME = ''
 TOKEN = real
 APP_URL = 'https://careerweek.herokuapp.com/' + TOKEN
